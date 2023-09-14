3eaadf0e710b587f01c29610223d9600d842ca70
CHAsencenge <958198376@qq.com>
2023-09-14 19:31:07 +0800

    图像处理库

--- TrRenderer/TrVulkanRenderer/TrVulkanUtil.h
+++ TrRenderer/TrVulkanRenderer/TrVulkanUtil.h
@@ -7,6 +7,9 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 // resource management
 #include <functional>
 #include <iostream>
