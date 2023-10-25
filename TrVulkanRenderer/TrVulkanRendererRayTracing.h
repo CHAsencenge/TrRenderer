caeabbd3639727482900dadde10621daa6e1c783
CHAsencenge <958198376@qq.com>
2023-10-26 02:18:30 +0800

    RT管线搭建 --- load model， create texture

--- TrVulkanRenderer/TrVulkanRendererRayTracing.h
+++ TrVulkanRenderer/TrVulkanRendererRayTracing.h
@@ -3,6 +3,8 @@
 #include "nvh/cameramanipulator.hpp"
 #include "nvvkhl/appbase_vk.hpp"
 #include "nvvk/context_vk.hpp"
+#include "nvvk/debug_util_vk.hpp"
+#include "nvvk/resourceallocator_vk.hpp"
 
 class TrVulkanRendererRayTracingBase : public TrVulkanRendererBase, public nvvkhl::AppBaseVk
 {
@@ -11,7 +13,7 @@ public:
 
     TrVulkanRendererRayTracingBase(uint32_t width, uint32_t height, const char* title);
     
-#pragma region Core
+#pragma region TrVulkanRendererBase
     
     void Run() override;
 
@@ -31,12 +33,16 @@ public:
 #pragma endregion
 
 
+#pragma region AppBaseVk
+
+    void setup(const VkInstance& instance, const VkDevice& device, const VkPhysicalDevice& physicalDevice, uint32_t graphicsQueueIndex) override;
+
+#pragma endregion 
+
+
 #pragma region nvvk
 
     void IsSurfaceSupportPresent();
-
-    // 
-    void Setup();
 
 #pragma endregion 
 
@@ -47,17 +53,35 @@ public:
 
 #pragma endregion
 
+#pragma region TrVulkanRendererRayTracingBase
+
+    void LoadModel(const std::string& filename, nvmath::mat4f transform = nvmath::mat4f(1));
+
+    void CreateTextureImages(const VkCommandBuffer cmdBuffer, const std::vector<std::string> textures);
+
+#pragma endregion 
+
 protected:
     nvmath::vec3f mEye = TrVulkanGlobalRT::camEye;
     
     nvmath::vec3f mCenter = TrVulkanGlobalRT::camCenter;
     
     nvmath::vec3f mUp = TrVulkanGlobalRT::camUp;
+    
 
     nvvk::Context mNvContext = {};
 
     VkSurfaceKHR mSurface;
 
     // DMA means device memory allocator
+    nvvk::ResourceAllocatorDma mResourceAllocDma;
+
+    nvvk::DebugUtil mDebugger; // can set object name, give label
+
+    VkFormat mOffscreenColorFormat {VK_FORMAT_R32G32B32A32_SFLOAT};
+
+    VkFormat mOffscreenDepthFormat {VK_FORMAT_X8_D24_UNORM_PACK32};
+
+    std::vector<nvvk::Texture> mTextures;
 };
 
